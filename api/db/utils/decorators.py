a6af663810d27b70a7cd5e4e46ad525d22d7cbd1
Goti-staniol <wkopinlinejon@gmail.com>
2025-05-23 23:22:04 +0300

    ✨ Update decorator
    [+] Update decorator in API

--- api/db/utils/decorators.py
+++ api/db/utils/decorators.py
@@ -4,17 +4,23 @@ from db import async_session
 from utils.logger import api_logger
 
 
-def connection(autocommit: bool = True):
+def connection(commit: bool = True, return_result: bool = False):
     def handler(func):
         @wraps(func)
-        async def wrapper(*args):
+        async def wrapper(*args, **kwargs):
             async with async_session() as session:
                 try:
-                    await func(session, *args)
-                    if autocommit:
+                    result = await func(session, *args, **kwargs)
+                    
+                    if commit:
                         await session.commit()
+                        
+                    if return_result:
+                        return result
+                    
                 except Exception as e:
                     api_logger.error(e)
                     await session.rollback()
+                return None
         return wrapper
     return handler
